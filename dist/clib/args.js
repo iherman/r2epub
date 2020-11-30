a1408a5b6bbf614340876771b71f8f08856b5554
Ivan Herman <ivan@ivan-herman.net>
2020-11-30 09:47:37 +0100

    Updated to latest node and typescript

--- dist/clib/args.js
+++ dist/clib/args.js
@@ -33,9 +33,6 @@
  *
  * @packageDocumentation
  */
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.get_book_configuration = void 0;
 /**
