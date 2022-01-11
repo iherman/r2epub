a610913b9bd3e4c09e83936bce477ce6eaa1bfb1
Ivan Herman <ivan@ivan-herman.net>
2022-01-11 16:39:08 +0100

    damn it...

--- src/lib/xhtml.ts
+++ src/lib/xhtml.ts
@@ -20,6 +20,9 @@ import { serializeToString } from 'xmlserializer';
 import * as jsdom            from 'jsdom';
 import { remove_entities }   from './utils';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const serialize = require("w3c-xmlserializer");
+
 
 
 /**
@@ -29,17 +32,29 @@ import { remove_entities }   from './utils';
  * @returns - Same content serialized as XHTML
  */
 export function convert(html: jsdom.JSDOM | string): string {
-    const convert_text = (html_text: string) :string => {
-        /*
-        The code is a bit ugly... it seems that the xhtml serializer relies on the specificities of the parse5 module output.
-        On the other hand, the output of that module does not seem to implement a bunch of DOM features that the rest of the code
-        relies on which means that it cannot be used elsewhere...
-        */
-        return '<!DOCTYPE html>' + serializeToString(parse(html_text))
+    // const convert_text = (html_text: string) :string => {
+    //     /*
+    //     The code is a bit ugly... it seems that the xhtml serializer relies on the specificities of the parse5 module output.
+    //     On the other hand, the output of that module does not seem to implement a bunch of DOM features that the rest of the code
+    //     relies on which means that it cannot be used elsewhere...
+    //     */
+    //     return '<!DOCTYPE html>' + serializeToString(parse(html_text))
+    // }
+
+    // const return_value = (typeof html === "string") ? convert_text(html) : convert_text(html.serialize());
+    // return remove_entities(return_value);
+
+
+    if (typeof html === "string" ) {
+        return html;
+    } else {
+        const almost = serialize(html.window.document.documentElement);
+        const almost_all: string[] = almost.split('\n');
+        almost_all[0] = '<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" xml:lang="en-US"><head>';
+        const retval = almost_all.join('\n');
+        return retval;
     }
 
-    const return_value = (typeof html === "string") ? convert_text(html) : convert_text(html.serialize());
-    return remove_entities(return_value);
 
     // // Last touch: the result should not include xml entities but only code numbers.
     // // The xhtml conversion should filter them out, but but it does not :-(
