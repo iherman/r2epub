b8ffabccb4f557aebcfe4f7569b18bda470d97e9
Ivan Herman <ivan@ivan-herman.net>
2025-05-29 16:27:59 +0200

    Using replaceAll

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -89,27 +89,6 @@ export function date_to_string(date :string) :string {
 }
 
 
-// /**
-//  * @hidden
-//  */
-// const entity_codes :string[][] = [
-//     ['&nbsp;', '&#160;'],
-//     ['&lt;',    '&#60;'],
-//     ['&gt;',    '&#62;'],
-//     ['&quot;',  '&#34;'],
-//     ['&apos;',  '&#39;'],
-//     ['&reg;',   '&#174;'],
-//     ['&pound;', '&#163;'],
-//     ['&yen;',   '&#165;'],
-//     ['&euro;',  '&#8364;'],
-//     ['&cent;',  '&#162;'],
-//     ['&mdash;', '&#8212;'],
-//     ['&ndash;', '&#8211;'],
-//     ['&emsp;',  '&#8195;'],
-//     ['&ensp;',  '&#8194;'],
-//     ['&thinsp;','&#8201;'],
-// ]
-
 /**
  * Filter XML entities in an xhtml code, and turn them into their equivalent hexadecimal Unicode point
  *
@@ -120,9 +99,7 @@ export function remove_entities(inp: string): string {
     let return_value = inp;
     for (const conversion of entity_codes) {
         const [entity, code] = conversion;
-        // This should be a return_value.replaceAll(entity,code), but that function is not
-        // implemented in node version 14.*
-        return_value = return_value.split(entity).join(code);
+        return_value = return_value.replaceAll(entity, code);
     }
     return return_value;
 }
